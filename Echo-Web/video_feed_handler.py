fbb5e15799dad3e5e070b604159242db6ea9f036
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2025-11-06 15:58:24 +0100

    Added video to robot space mapping

--- Echo-Web/video_feed_handler.py
+++ Echo-Web/video_feed_handler.py
@@ -113,6 +113,34 @@ def draw_landmarks_adaptive(frame, landmark_list, image_w, image_h, connections)
             except Exception:
                 continue
 
+#---------------------------------------------------------------
+
+#Maps a value from one range (in_min to in_max) to another range (out_min to out_max).
+#-------------Remap  helpers-----------------
+def remap_value(value, in_min, in_max, out_min, out_max):    
+    #Find the value as a percentage relative to the input range
+    in_span = in_max - in_min
+    in_percentage = (value - in_min) / in_span
+    print(f"Value: {value} \nin span: {in_span}, in percent: {in_percentage}%")
+    
+    #Apply the percentage to the output range
+    out_span = out_max - out_min
+    out_value = out_min + (in_percentage * out_span)
+    print(f"out span: {out_span}, out value: {out_value}")
+    
+    #Find real min/max
+    true_out_min = min(out_min, out_max)
+    true_out_max = max(out_min, out_max)
+
+    #Saftey check to max sure the value is withing acceptable range
+    if out_value < true_out_min:
+        out_value = true_out_min
+    if out_value > true_out_max:
+        out_value = true_out_max
+    
+    return int(out_value)
+#--------------------------------------------
+
 
 def print_data(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     try:
@@ -178,13 +206,26 @@ def print_data(result: HandLandmarkerResult, output_image: mp.Image, timestamp_m
             #print(f"{timestamp_ms}: wrist_world: raw=({pos_f[0]:.6f},{pos_f[1]:.6f},{pos_f[2]:.6f})")
             #print(f"{timestamp_ms}: velosity: ({vel_f[0]:.6f},{vel_f[1]:.6f},{vel_f[2]:.6f})")
 
-
+            #---CALIBRATE VIDEO SPACE TO ROBOT SPACE---
+            #print(f"CAMERA caleb: X: {pos_f[0]:.2f}, Y: {pos_f[1]:.2f}, Z: {pos_f[2]:.2f}")
+            #print(f"X: {pos_f[0]:.4f}")
+            
             #-------------SEND ROBOT ARM COMMANDS----------------
-            #TODO ADD all calcs and other stuff idk
-            base_angle = int(pos_f[0] * 180)
-            #print(f"X value: {base_angle}")
-            #Send to pico
-            send_angles_to_arm({0: base_angle})
+            #Calibration ranges
+            X_MIN, X_MAX = 0.01, 0.06 
+            Y_MIN, Y_MAX = 0.04, 0.10 
+            Z_MIN, Z_MAX = -0.01, 0.07
+            #Servo range
+            SERVO_MIN, SERVO_MAX = 0, 180
+
+            #---Mapping---
+            # X-axis (left/right) -> Base (joint 0)
+            base_angle = remap_value(pos_f[0], X_MIN, X_MAX, 180, 0)
+
+            #---Send to pico---
+            print(f"Base: {base_angle}")
+            send_angles_to_arm({0 : base_angle})
+            
             
         else:
             #print(f"{timestamp_ms}: wrist_world: N/A")
